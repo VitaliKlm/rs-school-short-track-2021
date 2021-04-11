4c2ac70838317ff6b5c1c6f8c4a9cc17195b42a2
VitaliKlm <kv37813@gmail.com>
2021-04-11 19:37:02 +0300

    feat: implement 06-mac-address algorithm

--- src/06-mac-address.js
+++ src/06-mac-address.js
@@ -12,8 +12,9 @@
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new Error('Not implemented');
+function isMAC48Address(n) {
+  const reg = /^[\dA-F]{2}[-][\dA-F]{2}[-][\dA-F]{2}[-][\dA-F]{2}[-][\dA-F]{2}[-][\dA-F]{2}$/;
+  return reg.test(n);
 }
 
 module.exports = isMAC48Address;
