4a341b340ebfd60196f8cb70cf1610876110b427
VitaliKlm <kv37813@gmail.com>
2021-04-12 10:29:49 +0300

    feat: implement 11-encode-line algorithm

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,27 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  // use reversed str for work with output as a stack,
+  // where the last elem is a counter of repeat symbols
+  const reverseArr = str.split('').reverse();
+  const reverseOutput = [];
+  let counter = 1;
+  reverseArr.forEach((elem, index) => {
+    if (reverseOutput[reverseOutput.length - 1] === elem) {
+      counter++;
+      if (index === reverseArr.length - 1) {
+        reverseOutput.push(counter);
+      }
+    } else {
+      if (counter > 1) {
+        reverseOutput.push(counter);
+        counter = 1;
+      }
+      reverseOutput.push(elem);
+    }
+  });
+  return reverseOutput.reverse().join('');
 }
 
 module.exports = encodeLine;
