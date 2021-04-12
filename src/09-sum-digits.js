ac21f873822776ca20c0f2ff2b1ea5f898e129c3
VitaliKlm <kv37813@gmail.com>
2021-04-12 08:56:22 +0300

    feat: implement 09-sum-digits algorithm

--- src/09-sum-digits.js
+++ src/09-sum-digits.js
@@ -10,8 +10,12 @@
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(n) {
+  let nStr = String(n);
+  while (nStr.length > 1) {
+    nStr = String(nStr.split('').reduce((a, b) => a + Number(b), 0));
+  }
+  return Number(nStr);
 }
 
 module.exports = getSumOfDigits;
